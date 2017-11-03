ceacd3181e6966895ddf54bed8f263bfe4fdb02e
Zhangbo Chen <czb128abc@163.com>
2017-11-03 18:19:12 +0800

    fix lint

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,5 @@
-'use strict';
+/* eslint-disable no-use-before-define */
+
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
@@ -7,7 +8,7 @@ process.env.NODE_ENV = 'production';
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   throw err;
 });
 
@@ -43,7 +44,7 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 // First, read the current file sizes in build directory.
 // This lets us display how much they changed later.
 measureFileSizesBeforeBuild(paths.appBuild)
-  .then(previousFileSizes => {
+  .then((previousFileSizes) => {
     // Remove all content but keep the directory so that
     // if you're in it, you don't end up in Trash
     fs.emptyDirSync(paths.appBuild);
@@ -58,14 +59,14 @@ measureFileSizesBeforeBuild(paths.appBuild)
         console.log(chalk.yellow('Compiled with warnings.\n'));
         console.log(warnings.join('\n\n'));
         console.log(
-          '\nSearch for the ' +
-            chalk.underline(chalk.yellow('keywords')) +
-            ' to learn more about each warning.'
+          `\nSearch for the ${ 
+            chalk.underline(chalk.yellow('keywords')) 
+          } to learn more about each warning.`
         );
         console.log(
-          'To ignore, add ' +
-            chalk.cyan('// eslint-disable-next-line') +
-            ' to the line before.\n'
+          `To ignore, add ${ 
+            chalk.cyan('// eslint-disable-next-line') 
+          } to the line before.\n`
         );
       } else {
         console.log(chalk.green('Compiled successfully.\n'));
@@ -93,7 +94,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
         useYarn
       );
     },
-    err => {
+    (err) => {
       console.log(chalk.red('Failed to compile.\n'));
       printBuildError(err);
       process.exit(1);
@@ -104,7 +105,7 @@ measureFileSizesBeforeBuild(paths.appBuild)
 function build(previousFileSizes) {
   console.log('Creating an optimized production build...');
 
-  let compiler = webpack(config);
+  const compiler = webpack(config);
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
       if (err) {
--- src/pages/music/components/Layout.jsx
+++ src/pages/music/components/Layout.jsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Layout, Icon } from 'antd';
+import { Layout } from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, Sider } = Layout;
 
 export default class LayoutView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  onSiderCollapse = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
   render() {
     return (
-      <Layout>
-        <Header>header</Header>
+      <Layout className="top-layout">
+        <Header>search</Header>
         <Layout>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onSiderCollapse}
+          >
+            Sider
+          </Sider>
           <Content>
-            main content
-            <Icon type="link" />
+            Content
           </Content>
         </Layout>
-        <Footer>footer</Footer>
       </Layout>
     );
   }
